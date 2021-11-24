efd69e2cc77ab40c0b3c55ed514d1671b0cc1d4f
Hovhannes <hovhannes.g@sdc.am>
2021-11-24 16:56:52 +0400

    add files

---
+++ app/js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+  // $(window).scroll(function () {
+  //   if ($(this).scrollTop() > 10) {
+  //     $(".header").addClass("with-bg");
+  //   } else {
+  //     $(".header").removeClass("with-bg");
+  //   }
+  // });
+
+  // if ($(window).scrollTop() > 1) {
+  //   $(".header").addClass("with-bg");
+  // } else {
+  //   $(".header").removeClass("with-bg");
+  // }
+
+  // Open mobile menu
+  // $(".burger").click(function () {
+  //   $(".mobile-menu").addClass("opened");
+  //   $("body").addClass("scroll-off");
+  // });
+
+  // Close mobile menu
+  // $(".close-btn").click(function () {
+  //   $(".mobile-menu").removeClass("opened");
+  //   $("body").removeClass("scroll-off");
+  // });
+});
